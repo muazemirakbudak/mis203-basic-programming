5d43f1426ba453e0590bc4fb654c60dff915fcef
muazemirakbudak <muazemir312@gmail.com>
2026-09-22 18:45:51 +0300

    Update comments in student_profile.py

--- week01/student_profile.py
+++ week01/student_profile.py
@@ -1,10 +1,10 @@
-# 1. Aşama: Kullanıcıdan bilgileri alma
+# Kullanıcıdan bilgileri alma
 name = input("Enter your name: ")
 department = input("Enter your department: ")
 age = input("Enter your age: ")
 career_goal = input("Enter your career goal: ")
 
-# 2. Aşama: Öğrenci profilini ekrana yazdırma
+# Öğrenci profilini yazdırma
 print("\n--- Student Profile ---")
 print(f"Name: {name}")
 print(f"Department: {department}")
